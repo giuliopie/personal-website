d6958c0bd22f2d206bcce7e493d0d153e904a46a
giuliopie <giuliopiepoli.dev@gmail.com>
2022-04-24 13:33:21 +0200

    Init project

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import { createApp } from "vue";
+import { createWebHistory, createRouter } from "vue-router";
+
+// styles
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@/assets/styles/tailwind.css";
+
+// mouting point for the whole app
+
+import App from "@/App.vue";
+
+// views without layouts
+
+import Profile from "@/views/Profile.vue";
+import Index from "@/views/Index.vue";
+
+// routes
+
+const routes = [
+  {
+    path: "/profile",
+    component: Profile,
+  },
+  {
+    path: "/",
+    component: Index,
+  },
+  { path: "/:pathMatch(.*)*", redirect: "/" },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
